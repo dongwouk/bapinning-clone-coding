de7bdd769c35cb5af6b45d95bab9b09bde390ea5
kwon <kwondw30@gmail.com>
2024-03-07 15:41:57 +0900

    first upload

---
+++ src/main/java/com/exam/BootTemplate1Application.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTemplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTemplate1Application.class, args);
+	}
+
+}
